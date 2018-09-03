5d97ef8cd5ba0946d116eb955f22076939499d26
Ramesh <ramesh@rsageventures.com>
2018-09-03 22:21:00 +0530

    OK

--- src/main/java/com/rs/app/App.java
+++ src/main/java/com/rs/app/App.java
@@ -1,22 +1,34 @@
 package com.rs.app;
 
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
 public class App {
 	public static void main(String[] args) {
-		int[] elements = { 10, 0, 56, 23, 7, 8, 5 };
+		int[] elements = { 10, 0, 56, 2, 23, 7, 8, 5, 2 };
 		Sample sample = new Sample();
-		int min = sample.findMin(elements);
-		System.out.println(min);
+		// int min = sample.findMin(elements);
+		// System.out.println(min);
+		sample.findThreeDistinictSmallestElements(elements);
 	}
 }
 
 class Sample {
 	public int findMin(int[] elements) {
-		int min = elements[0];
-		for (int i = 1; i < elements.length; i++) {
-			if (min > elements[i]) {
-				min = elements[i];
-			}
+		OptionalInt oi = IntStream.of(elements).parallel().min();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
 		}
-		return min;
+		if (oi.isPresent()) {
+			System.out.println(oi.getAsInt());
+		}
+		System.out.println("Thread: " + Thread.currentThread().getName());
+		return oi.getAsInt();
+	}
+
+	public void findThreeDistinictSmallestElements(int[] elements) {
+		IntStream.of(elements).distinct().sorted().limit(3).boxed().forEach(System.out::println);
 	}
 }
